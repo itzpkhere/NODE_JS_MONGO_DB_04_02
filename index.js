59bbeaacd3d5ec0dfddfbea8c4fe992b67ca2f5c
itzpkhere <74782373+itzpkhere@users.noreply.github.com>
2022-02-07 12:32:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/login',(req,res)=>{
 app.post('/signup',(req,res)=>{
     const user = new user_records({name : 'b',city : 'Bombay',email : 'b@b',password : 'b'});
     user.save().then(()=>{
-        const token = jwt.sign({ user_id: user._id },"my_secret_key",{ expiresIn: "5m"});
+        const token = jwt.sign({ user_id: user._id },"my_secret_key");
         user.token = token;
         res.status(201).json({
             user : user,
@@ -41,10 +41,14 @@ app.post('/signup',(req,res)=>{
 app.post('/login',async(req,res)=>{ 
     const user = await user_records.findOne({ email : 'a@a', password : 'a'});
     if(user){
-        const token = jwt.sign({ user_id : user._id},"my_secret_key",{ expiresIn: "5m"});
+        const token = jwt.sign({ user_id : user._id},"my_secret_key");
         user.token = token;
         // res.status(200).json(user); 
-        res.status(200).cookie("token",token,{ maxAge : 20000, httpOnly : true }).json({ user });
+        res.status(200).cookie("token",token,{ maxAge : 30 * 1000, httpOnly : true }).json(
+            { 
+                user,
+                message : 'Logged In!' 
+            });
     }else{
         res.status(400).send("Invalid Credentials");
     }
@@ -55,21 +59,35 @@ function verifyToken(req,res,next){
     const token =  req.cookies.token || req.body.token;
     if(!token){
         return res.status(403).send('Forbidden');
-    }
-    try{
-        const decoded = jwt.verify(token,'my_secret_key');
-        req.user = decoded;
-    }catch(err){
-        res.status(401).send("Invalid Token!");
+    }else{
+        try{
+            const decoded = jwt.verify(token,'my_secret_key');
+            req.user = decoded;
+            console.log(decoded);
+        }catch(err){
+            res.status(401).send("Invalid Token!");
+        }
     }
     next();
 }
- 
+
 app.get('/protected',verifyToken,(req,res,decoded)=>{
         res.status(200).json({ 
             message : 'You are lucky if you can see this page!'
         })
 })
+
+app.get('/get-account-details',verifyToken, async(req,res,decoded)=>{
+    const user = await user_records.findOne({ user : decoded.user_id });
+    user.password = undefined;
+    res.status(200).json({ user })
+    // res.json({
+    //     name : user.name,
+    //     email : user.email,
+    //     city : user.city,
+    // })
+})
+
 
 
 app.get("/logout", (req, res) => {
